66ec4168b22459a6884e5c509ddc0c12e46c341d
WolfOWI <97434628+WolfOWI@users.noreply.github.com>
2024-02-27 07:11:58 +0200

    Header section complete
    Header area for landing page completed

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import Header from "./components/Header/Header.jsx";
 
 function App() {
   return (
-    <div className="">
+    <div>
       {/* Header Section */}
-      <div className="bg-gradient-to-r from-blue-400 to-green-400">
-        {/* Content */}
+      <div className="bg-gradient-to-r from-blue-400 to-green-400 rounded-b-3xl">
+        {/* Header Content */}
         <div className="m-auto w-11/12 sm:w-4/5 lg:w-3/5">
           <NavBar />
           <Header />
