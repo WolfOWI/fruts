e4bd66a61dae4c05cb8a860c945f8c4636d95726
WolfOWI <97434628+WolfOWI@users.noreply.github.com>
2024-03-06 13:43:52 +0200

    Side fruits added to compare header
    Added side fruits on compare page on the header.

--- src/components/pages/CompareComponents/CompareHeader.jsx
+++ src/components/pages/CompareComponents/CompareHeader.jsx
@@ -9,16 +9,33 @@ import orangeFull from "../../../assets/img/fruits/orange.png";
 
 function Header() {
   return (
-    <div className="w-full pt-5 pb-10">
+    <div className="relative w-full pt-5 pb-10">
+      {/* Fruit Left */}
+      <div>
+        <img
+          src={appleFull}
+          alt="-"
+          className="absolute w-72 hidden sm:block sm:-left-56 md:-left-48 translate-y-10"
+        />
+      </div>
+      {/* Fruit Right */}
+      <div>
+        <img
+          src={orangeFull}
+          alt="-"
+          className="absolute w-72 hidden sm:block sm:-right-56 md:-right-48 translate-y-10"
+        />
+      </div>
+      {/* Content Center */}
       <div className="text-center">
         <h1 className="font-head text-slate-50 text-5xl font-bold pb-2">compare</h1>
         <p className="text-sm font-body mix-blend-overlay">nutrients per 100g</p>
       </div>
-      <div className="flex justify-center mt-2">
-        <div className="w-2/6">
+      <div className="flex justify-center items-center mt-2 flex-col sm:flex-row">
+        <div className="w-full sm:w-2/6">
           <DropdownFruits />
         </div>
-        <div className="w-2/6 ml-3">
+        <div className="w-full sm:w-2/6 sm:ml-3">
           <DropdownFruits />
         </div>
       </div>
