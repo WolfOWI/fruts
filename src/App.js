756c637ddc90d73e07dfc655d524a13a64fba284
WolfOWI <97434628+WolfOWI@users.noreply.github.com>
2024-03-21 18:16:27 +0200

    Implemented scroll to top
    When navigating to different pages, the page will start at the top.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+// Import
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop.jsx";
 
 // Import Pages
 import LandingPage from "./components/pages/LandingPage.jsx";
@@ -8,6 +10,7 @@ import ComparePage from "./components/pages/ComparePage.jsx";
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<LandingPage />} exact />
         <Route path="/compare" element={<ComparePage />} exact />
