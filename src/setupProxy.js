beb695e8946e648d68711f2104c72c2020c46e32
WolfOWI <97434628+WolfOWI@users.noreply.github.com>
2024-10-03 17:54:48 +0200

    AgriAPI Fix

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,19 +1,16 @@
-// A Proxy Server for the EU-AgriData API
-// (Due to CORS errors, as the EU-AgriData API is fully open, free to use, and does not have API keys or user verification)
-
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
     "/api",
-    // The "/api" path is used for proxying API requests (shortcut)
     createProxyMiddleware({
-      // The target host of the API
       target: "https://ec.europa.eu",
-      // Set to true for virtual hosted sites
       changeOrigin: true,
       pathRewrite: { "^/api": "/agrifood/api/fruitAndVegetable/prices" },
       logLevel: "debug",
+      headers: {
+        "Access-Control-Allow-Origin": "*", // Add this header to prevent CORS issues
+      },
     })
   );
 };
